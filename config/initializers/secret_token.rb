d0f19d635502a7893841970871385e5cd305ecfd
Alexander Kazakov <alexander@kazakov.mobi>
2013-02-04 11:56:20 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Choresapp::Application.config.secret_token = '5a85cf48711109493706fd4588cd73b627379521607c16787f9be5defe2aaeb0b6b9e9ea599c826e36e972d56e83723b751379e0377ad1fffe5eb023311e0264'
